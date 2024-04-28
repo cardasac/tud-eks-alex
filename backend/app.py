c03f47068ee172a265c63134e5cc456c1b487bfa
Alexandru Constantin Cardas <calexc@alviralex.com>
2024-04-28 03:31:28 +0100

    Add formatter

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,16 @@ import psycopg
 app = Flask(__name__)
 DB_URI = os.getenv("DB_URI")
 
+
 @app.post("/<text>")
 def add_product(text):
     with psycopg.connect(DB_URI) as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO products (description) VALUES (%s)",
-                (text,))
+            cur.execute("INSERT INTO products (description) VALUES (%s)", (text,))
             conn.commit()
 
-    return jsonify(status = "success"), 200
+    return jsonify(status="success"), 200
+
 
 @app.get("/")
 def view_products():
@@ -25,16 +25,16 @@ def view_products():
 
             return cur.fetchall()
 
+
 @app.delete("/<id>")
 def delete_product(id):
     with psycopg.connect(DB_URI) as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                "DELETE FROM products WHERE id = %s",
-                (id,))
+            cur.execute("DELETE FROM products WHERE id = %s", (id,))
             conn.commit()
 
-    return jsonify(status = "success"), 200
+    return jsonify(status="success"), 200
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080, use_reloader=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, use_reloader=True)
