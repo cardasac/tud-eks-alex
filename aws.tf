b3c9fe8979b9cc8457f79ab247c017badea829ab
Alexandru Constantin Cardas <calexc@alviralex.com>
2024-04-28 16:43:05 +0100

    Fix

--- aws.tf
+++ aws.tf
@@ -22,7 +22,7 @@ module "vpc" {
 
   private_subnets         = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets          = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  map_public_ip_on_launch = true
+  # map_public_ip_on_launch = true
   enable_nat_gateway      = true
   single_nat_gateway      = true
   enable_dns_hostnames    = true
--- frontend/src/app/actions.js
+++ frontend/src/app/actions.js
@@ -2,15 +2,18 @@
 
 import { revalidatePath } from "next/cache";
 
-export async function createProduct(
-  prevState,
-  formData,
-) {
-  const data = {product: formData.get("product")};
-  console.log(data)
+export async function createProduct(prevState, formData) {
+  const data = { product: formData.get("product") };
+
+  if (!data.product) {
+    return { message: "Failed to create todo" };
+  }
 
   try {
-    await fetch(`http://${process.env.PRODUCT_API}:8080/${data["product"]}`, {method: "POST", cache: 'no-store'}).then(response => response.text());
+    await fetch(`http://${process.env.PRODUCT_API}:8080/${data["product"]}`, {
+      method: "POST",
+      cache: "no-store",
+    }).then((response) => response.text());
 
     revalidatePath("/");
     return { message: `Added product ${data.product}` };
@@ -19,21 +22,18 @@ export async function createProduct(
   }
 }
 
-export async function deleteProduct(
-  prevState,
-  formData,
-) {
+export async function deleteProduct(prevState, formData) {
   const data = {
     id: formData.get("id"),
     text: formData.get("text"),
   };
-  console.log(data)
 
   try {
-    await fetch(`http://${process.env.PRODUCT_API}:8080/${data["id"]}`, {method: "DELETE", cache: 'no-store'}).then(response => response.text());
-
+    await fetch(`http://${process.env.PRODUCT_API}:8080/${data["id"]}`, {
+      method: "DELETE"}).then((response) => response.text());
+    
     revalidatePath("/");
-    return { message: `Deleted product ${data.text}` };
+    return { message: `Deleted product ${data["text"]}` };
   } catch (e) {
     return { message: "Failed to delete product" };
   }
